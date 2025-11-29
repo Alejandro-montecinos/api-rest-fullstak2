29f5e099828d8f5034964ad917ebee9d1347bfab
alejandro montecinos <alejandromontecino002@gmail.com>
2025-11-29 13:59:09 -0300

    se cambia de apuestas a pokemon

--- src/main/java/com/duocuc/api/app/kotling/apuestas/controller/UsuarioController.java
+++ src/main/java/com/duocuc/api/app/kotling/apuestas/controller/UsuarioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/apuestas/usuarios")
+@RequestMapping("/api/pokemon/usuarios")
 @RequiredArgsConstructor
 public class UsuarioController {
 
